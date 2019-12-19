644b6dc05069663d708b3ac0a62df4a7a54ad82a
yosraf <yosrafatnassi6@gmail.com>
2019-12-19 11:24:24 +0100

    task ui aded

--- src/app/Screens/task/task.page.ts
+++ src/app/Screens/task/task.page.ts
@@ -10,23 +10,78 @@ export class TaskPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.laod();
+  }
+  laod(){
     this.data=[
       {
         "id":'1',
         "name":"crud project",
         "description":"create a web app that ...",
         "state":"to do"
-      }]
+      },
+      {
+        "id":'1',
+        "name":"crud project",
+        "description":"create a web app that ...",
+        "state":"doing"
+      },
+      {
+        "id":'1',
+        "name":"crud project",
+        "description":"create a web app that ...",
+        "state":"done"
+      }
+    ]
   }
   color(progress){
     if(progress=="to do"){
-      return "#feca57";
+      return "danger";
     }
     if(progress=="doing"){
-      return "#ce82ed";
+      return "tertiary";
     }
     if(progress=="done"){
-      return "#8e44ad";
+      return "warning";
     }
+   }
+   filter(n){
+     var fil:any=[];
+    if(n==0){
+      this.laod();
+    }
+    if(n==1){
+      this.laod();
+      this.data.forEach(element => {
+        if(element['state']=="to do"){
+          fil.push(element);
+        }
+        
+      });
+      this.data=fil;
+    }
+    if(n==2){
+      this.laod();
+      this.data.forEach(element => {
+        if(element['state']=="doing"){
+          fil.push(element);
+        }
+        
+      });
+      this.data=fil;
+
+    }
+    if(n==3){
+      this.laod();
+      this.data.forEach(element => {
+        if(element['state']=="done"){
+          fil.push(element);
+        }
+        
+      });
+      this.data=fil;
+
+    }
+
    }
 }
