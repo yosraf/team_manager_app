a0b4821b6a0da626c98ef5a57c152ec0abe02a96
yosraf <yosrafatnassi6@gmail.com>
2020-01-17 16:18:11 +0100

    add dev service

---
+++ src/app/Services/task-dev.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import {AngularFirestore }from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskDevService {
+
+  constructor() { }
+}
