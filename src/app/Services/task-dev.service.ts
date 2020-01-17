2bdaf9806bf7f51c1cf2f26c08a9b490af55edcf
yosraf <yosrafatnassi6@gmail.com>
2020-01-17 20:23:14 +0100

    added getprojects and get tasks

--- src/app/Services/task-dev.service.ts
+++ src/app/Services/task-dev.service.ts
@@ -6,6 +6,79 @@ import {AngularFirestore }from '@angular/fire/firestore';
   providedIn: 'root'
 })
 export class TaskDevService {
+  
+  ids:any=[];
+  constructor(private afs: AngularFirestore) { 
+  }
 
-  constructor() { }
+  getProjects(){
+    let projects_id=[];
+   
+    let projects=[];
+    
+    let uid=firebase.auth().currentUser.uid;
+    this.afs.collection("projects").get().subscribe(res=>{
+     res.forEach(doc=>{
+      
+       this.afs.collection("projects").doc(doc.id).collection("tasks").get().forEach(res=>{
+         
+       
+        res.docs.forEach(d=>{
+          
+          
+          var obj = JSON.parse(JSON.stringify(d.data()));
+          
+          if(obj['person']==uid){
+            
+            projects_id.push(doc.id)
+            this.afs.collection("projects").doc(doc.id).get().forEach(e=>{
+             
+               var obj = JSON.parse(JSON.stringify(e.data()));
+              projects.push(obj);
+              
+            })           
+          } 
+          
+
+        })
+       } )
+      
+       
+     })
+    })
+   
+
+    
+   
+    return projects;
+    
+  }
+ 
+  getTasks(id){
+   
+    let projects=[]
+    let uid=firebase.auth().currentUser.uid;
+    this.afs.collection("projects").doc(id).collection("tasks").get().forEach(res=>{
+         
+      res.docs.forEach(d=>{
+        
+       
+        var obj = JSON.parse(JSON.stringify(d.data()));
+        
+        if(obj['person']==uid){
+          projects.push(obj)
+          console.log(obj);
+        } 
+        
+          
+        
+
+      })
+     } )
+    return projects;
+    
+  }
+
+  
+  
 }
